32963a32428f711888a2bf4500d78707a22fc851
jbb213 <jbb213@lehigh.edu>
2015-03-04 03:38:12 +0000

    updated MakeSymbols

--- hw06/MakeSymbols.java
+++ hw06/MakeSymbols.java
@@ -1,23 +1,23 @@
 //Jimmy Bradshaw 
-//Homework #5 
+//Homework #6 
 //2/24/15
 
+    public class MakeSymbols {  //Name A Class
+    public static void main(String[]args) {  //Main Method 
 
-  
-    
-    public class MakeSymbols { 
-    public static void main(String[]args) {
+    int limiteven=0; //Increasing Function For Even Numbers 
+    int limitodd=0; //Increasing Function For Odd Numbers 
 
-    int limiteven=0;
-    int limitodd=0; 
-
-    int number=(int)(Math.random() * (100) + 0); 
+    int number=(int)(Math.random() * (100) + 0); //Generating A Random Number 
+   
+   //Seeing if the number generated is even and proceding to do a do while loop  
     if ((number%2)==0) { 
         do {System.out.print("*");
             limiteven ++; 
     }
         while (limiteven < number);
-    
+   
+   //Seeing if the number generated is odd and proceding to do a do while loop 
     if ((number%2)==1) { 
         do {System.out.print("&");
         limitodd ++; 
